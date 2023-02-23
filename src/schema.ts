a53d184e4eada63981c3236beef1a952053ae36f
Nick Scott <46829523+nicholas-scott@users.noreply.github.com>
2023-02-23 17:40:58 -0500

    Added Fields for schema

--- src/schema.ts
+++ src/schema.ts
@@ -4,15 +4,34 @@ type Query {
 }
 
 type Map {
+    file_name: ID!
+
+    dateAdded: String!
+    description: String!
+    isOfficial: Boolean!
+    isPyroland: Boolean!
+    map_name: String!
+
     developers: [MapMaker!]!
     mapType: MapType!
 }
 
 type MapType{
+    prefix: ID!
+    
+    dateAdded: String!
+    isOfficial: Boolean!
+    typeName: String!
+
     maps: [Map!]!
 }
 
 type MapMaker{
+    id: ID!
+
+    link: String!
+    name: String!
+
     maps: [Map!]!
 }
 `
