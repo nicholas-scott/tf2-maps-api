85d70a2bedf8d036c90cbb28334308df5b0ba1c6
Nick Scott <46829523+nicholas-scott@users.noreply.github.com>
2023-02-23 16:49:28 -0500

    Defined basic types and their relationships.
    These are types I want to be queriable in my api.

--- src/schema.ts
+++ src/schema.ts
@@ -2,6 +2,19 @@ const typeDefs = `
 type Query {
     hello: String
 }
+
+type Map {
+    developers: [MapMaker!]!
+    mapType: MapType!
+}
+
+type MapType{
+    maps: [Map!]!
+}
+
+type MapMaker{
+    maps: [Map!]!
+}
 `
 
 export default typeDefs
