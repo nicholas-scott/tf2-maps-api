3f717432e78a88c7e5779ad9e9484cdbe6548f08
Nick Scott <46829523+nicholas-scott@users.noreply.github.com>
2023-03-08 21:18:19 -0500

    fixed typing on userId from json webtoken

--- src/util/getUserFromToken.ts
+++ src/util/getUserFromToken.ts
@@ -2,7 +2,7 @@ import JWT from "jsonwebtoken"
 
 export function getUserFromToken(token: string) {
 	try {
-		return JWT.verify(token, "PLACEMENT_SIGNATURE") as { userId: number }
+		return JWT.verify(token, "PLACEMENT_SIGNATURE") as { userId: string }
 	} catch (error) {
 		return null
 	}
